087c9be8f67f989914f284c8876e90442674940b
Akram-Adam <akramadam050@gmail.com>
2024-06-30 19:22:01 +0000

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,13 @@
-# set up your client SSH configuration file so that you can connect to a server without typing a password.
-file { '/home/kira/.ssh/config':
-  ensure => file,
-  content => @("EOF"),
-    Host myserver
-      HostName 54.173.122.164  # Replace with your server's IP or hostname
-      User ubuntu               # Replace with your server's username
-      IdentityFile ~/.ssh/school
-      PasswordAuthentication no
-    EOF,
-  owner => 'kira',     # Replace with your username
-  group => 'kira',     # Replace with your group if necessary
-  mode => '0600',               # Ensure correct permissions on the file
+# Set up your client SSH configuration file so that you can connect to a server without typing a password
+
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  ensure => present,
+}
+
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  ensure => present,
 }
