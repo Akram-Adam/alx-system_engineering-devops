32cdffe832b63dcbaeb530543b8865379bae71ac
Akram-Adam <akramadam050@gmail.com>
2024-08-14 22:42:59 +0000

    what

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,24 +2,49 @@
 # Author: Akram Adam
 # email:  <akramadam050@gmail.com>
 
-# Using Puppet| Install Nginx server, setup and configuration
+# Using Puppet| Install Nginx server, setup and configuration 
 
-package { 'nginx':
-  ensure => 'installed'
+class { 'nginx': 
+  package_ensure => 'present',
+  service_ensure => 'running',
+  service_enable => true,
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  notify  => Service['nginx'],
+}
+
+file { '/etc/nginx/sites-enabled/default':
+  ensure => link,
+  target => '/etc/nginx/sites-available/default',
 }
 
 file { '/var/www/html/index.html':
-  content => 'Hello World',
-}
-
-file_line { 'redirection-301':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  ensure  => file,
+  content => 'Hello World!',
 }
 
 service { 'nginx':
-  ensure  => running,
+  ensure => running,
+  enable => true,
   require => Package['nginx'],
+}
+
+nginx::resource::vhost { 'default':
+  ensure   => present,
+  listen_port => 80,
+  www_root => '/var/www/html',
+}
+
+file { '/etc/nginx/conf.d/redirect.conf':
+  ensure  => file,
+  content => 'server {
+                listen 80;
+                location /redirect_me {
+                  return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+                }
+              }',
+  notify  => Service['nginx'],
 }
